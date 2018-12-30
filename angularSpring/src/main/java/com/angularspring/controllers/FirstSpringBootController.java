c2062b866022e6216806fca24e9be1f92c3816a0
Bruno Batista <bbnsdevelop@gmail.com>
2018-12-30 18:21:37 -0200

    primeira url

---
+++ angularSpring/src/main/java/com/angularspring/controllers/FirstSpringBootController.java
@@ -0,0 +1,17 @@
+package com.angularspring.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class FirstSpringBootController {
+	
+	
+	@GetMapping("/hello")
+	public String showTeste() {
+		return "Hello, first spring boot project";
+	}
+
+}
